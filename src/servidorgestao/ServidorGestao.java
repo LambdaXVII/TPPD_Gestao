3743fd2c318285b2b3143713ea6802078472f7b3
LambdaXVII <rangernud@gmail.com>
2017-12-31 00:04:42 +0000

    Servidor de Gestao concorrente a enviar o tempo aos clientes

--- src/servidorgestao/ServidorGestao.java
+++ src/servidorgestao/ServidorGestao.java
@@ -1,22 +1,111 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package servidorgestao;
 
-/**
- *
- * @author range
- */
-public class ServidorGestao {
+import java.util.*;
+import java.net.*;
+import java.io.*;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        // tudo a funcionar
+
+
+class AtendeCliente extends Thread
+{
+    Socket socketToClient;
+    int myId;
+    
+    public AtendeCliente(Socket s, int id)
+    {
+        socketToClient = s;
+        myId = id;        
     }
     
+    @Override
+    public void run()
+    {
+        String request, timeMsg;
+        Calendar calendar;  
+        BufferedReader in;
+        PrintWriter out;
+        
+        try{
+            out = new PrintWriter(socketToClient.getOutputStream());
+            in = new BufferedReader(new InputStreamReader(socketToClient.getInputStream()));
+
+            request = in.readLine();
+
+            if(request == null){ //EOF
+                socketToClient.close();
+                System.out.println("<Thread_" + myId + "> "
+                        + "Ligacacao encerrada.");
+                return;
+            }
+
+            System.out.println("<Thread_" + myId + 
+                    "> Recebido \"" + request.trim() + "\" de " + 
+                    socketToClient.getInetAddress().getHostAddress() + ":" + 
+                    socketToClient.getPort());
+
+
+            if(request.equalsIgnoreCase(ServidorGestao.TIME_REQUEST)){
+
+                //Constroi a resposta terminando-a com uma mudanca de linha
+                calendar = GregorianCalendar.getInstance();
+                timeMsg = calendar.get(GregorianCalendar.HOUR_OF_DAY)+":"+ 
+                        calendar.get(GregorianCalendar.MINUTE)+":"+
+                        calendar.get(GregorianCalendar.SECOND);
+
+                //Envia a resposta ao cliente
+                out.println(timeMsg);
+                out.flush();
+            }
+
+        }catch(IOException e){
+            System.out.println("<Thread_" + myId + "> Erro na comunicação como o cliente " + 
+                    socketToClient.getInetAddress().getHostAddress() + ":" + 
+                        socketToClient.getPort()+"\n\t" + e);
+        }finally{
+            try{
+                socketToClient.close();
+            }catch(IOException e){}
+        }
+    }
+}
+
+public class ServidorGestao {
+ public static final int MAX_SIZE = 256;
+    public static final String TIME_REQUEST = "TIME";
+    
+    private ServerSocket socket;
+
+    public ServidorGestao(int listeningPort) throws IOException
+    {        
+        socket = new ServerSocket(listeningPort);
+    }
+    
+    public final void processRequests() throws IOException
+    {
+        int threadId = 1;
+        Socket toClientSocket;
+        
+        if(socket == null){
+            return;
+        }
+        
+        System.out.println("Concurrent TCP Time Server iniciado no porto " + socket.getLocalPort() + " ...");
+        
+        while(true){     
+            toClientSocket = socket.accept();            
+            new AtendeCliente(toClientSocket, threadId++).start();            
+        }
+    }
+           
+    public static void main(String[] args) throws IOException 
+    {                
+        if(args.length != 1){
+            System.out.println("Sintaxe: java TcpTimeServer listeningPort");
+            return;
+        }
+                
+        ServidorGestao tcpTimeServer = new ServidorGestao(Integer.parseInt(args[0]));
+        tcpTimeServer.processRequests();            
+    }
 }
