6ea0d45bca3699ac92bd8d91396ece2c15ee0dd2
LambdaXVII <rangernud@gmail.com>
2018-01-04 18:46:00 +0000

    commit update

--- src/servidorgestao/ServidorGestao.java
+++ src/servidorgestao/ServidorGestao.java
@@ -6,6 +6,93 @@ import java.net.*;
 import java.io.*;
 
 
+class AtendeClientesUDP extends Thread {
+public static final int MAX_SIZE = 256;
+    public static final String TIME_REQUEST = "TIME";
+    
+    private DatagramSocket socket;
+    private DatagramPacket packet; //para receber os pedidos e enviar as respostas
+    private boolean debug;
+
+    public AtendeClientesUDP(int listeningPort, boolean debug) throws SocketException 
+    {
+        socket = null;
+        packet = null;
+        socket = new DatagramSocket(listeningPort);
+        this.debug = debug;
+    }
+    
+    public String waitDatagram() throws IOException
+    {
+        String request;
+        
+        if(socket == null){
+            return null;
+        }
+        
+        packet = new DatagramPacket(new byte[MAX_SIZE], MAX_SIZE);
+        socket.receive(packet);
+        request = new String(packet.getData(), 0, packet.getLength());
+        
+        if(debug){
+            System.out.println("Recebido \"" + request + "\" de " + 
+                    packet.getAddress().getHostAddress() + ":" + packet.getPort());
+        }
+        return request;
+    }
+    
+    
+    public void processRequests() throws IOException
+    {
+        String receivedMsg, timeMsg;
+        Calendar calendar;        
+        
+        if(socket == null){
+            return;
+        }
+        
+        if(debug){
+            System.out.println("UDP Time Server iniciado...");
+        }
+        
+        while(true){
+            
+            receivedMsg = waitDatagram();
+            
+            if(receivedMsg == null){
+                continue;
+            }
+            
+            if(!receivedMsg.equalsIgnoreCase(TIME_REQUEST)){
+                continue;
+            }
+            
+            calendar = GregorianCalendar.getInstance();
+            timeMsg = calendar.get(GregorianCalendar.HOUR_OF_DAY)+":"+ 
+                    calendar.get(GregorianCalendar.MINUTE)+":"+calendar.get(GregorianCalendar.SECOND);
+            
+            packet.setData(timeMsg.getBytes());
+            packet.setLength(timeMsg.length());
+            
+            //O ip e porto de destino ja' se encontram definidos em packet
+            socket.send(packet);
+            
+        }
+    }
+    
+    public void closeSocket()
+    {
+        if(socket != null){
+            socket.close();
+        }
+    }
+
+}
+
+
+
+
+
 
 class AtendeCliente extends Thread
 {
@@ -96,9 +183,12 @@ public class ServidorGestao {
         
         while(true){     
             toClientSocket = socket.accept();            
-            new AtendeCliente(toClientSocket, threadId++).start();            
+            new AtendeCliente(toClientSocket, threadId++).start();  
+            
         }
     }
+    
+   
            
     public static void main(String[] args) throws IOException 
     {                
@@ -110,7 +200,32 @@ public class ServidorGestao {
         System.out.println("Endereço IP: "+InetAddress.getLocalHost().toString()); 
         System.out.println("Porto TCP para os clientes: "+args[0]);
         
-        ServidorGestao tcpTimeServer = new ServidorGestao(Integer.parseInt(args[0]));
-        tcpTimeServer.processRequests();            
+      
+        // Trata pedidos UDP dos Servidores de Jogo
+        AtendeClientesUDP atendeUDP = new AtendeClientesUDP(8000,true);  
+        
+       
+         try{
+            
+            atendeUDP.processRequests();
+            
+        }catch(NumberFormatException e){
+            System.out.println("O porto de escuta deve ser um inteiro positivo.");
+        }catch(SocketException e){
+            System.out.println("Ocorreu um erro ao nível do socket UDP:\n\t"+e);
+        }catch(IOException e){
+            System.out.println("Ocorreu um erro no acesso ao socket:\n\t"+e);
+        }finally{
+            if(atendeUDP != null){
+                atendeUDP.closeSocket();
+            }
+         }
+         
+         
+        // Instancia Servidor de Gestão
+        //ServidorGestao tcpTimeServer = new ServidorGestao(Integer.parseInt(args[0]));
+       
+        // Trata de pedidos TCP -> vindos dos clientes
+       // tcpTimeServer.processRequests();            
     }
 }
